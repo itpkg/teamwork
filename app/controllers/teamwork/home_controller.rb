18c57e693e9427010000691d4eb87c9157896e02
flamen <jitang.zheng@gmail.com>
2015-03-12 00:31:39 -0700

    remove controllers.js.erb

--- app/controllers/teamwork/home_controller.rb
+++ app/controllers/teamwork/home_controller.rb
@@ -2,8 +2,6 @@ require_dependency 'teamwork/application_controller'
 
 module Teamwork
   class HomeController < ApplicationController
-    protect_from_forgery except: [:controllers]
-
     def index
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Teamwork::Engine.routes.draw do
 
   get 'home'=>'home#index'
-  get 'controllers'=> 'home#controllers', format: :js
 
   resources :projects
   resources :stories
@@ -10,7 +9,6 @@ Teamwork::Engine.routes.draw do
   resources :documents
   resources :calendars
 
-  get 'excels/controllers', format: :js
   resources :excels do
 
   end
